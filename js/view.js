57f9553cad432842e3a27a04c5f07f0087dd8589
nicholls314159 <nicholls3.14159@gmail.com>
2016-06-05 23:08:13 -0700

    Update view.js

--- js/view.js
+++ js/view.js
@@ -91,7 +91,7 @@ function loadParamsFromURL() {
 function generateVideoViewer(){
     var div = $('<div>');
     div.addClass('videoPlayer');
-    var embeddedVideoPlayer = $('<iframe width="700" height="393" src="https://www.youtube.com/embed/'+viewObject.inputVideoID+'" frameborder="0" allowfullscreen></iframe>');	
+    var embeddedVideoPlayer = $('<iframe width="700" height="393" src="https://www.youtube.com/embed/'+viewObject.inputVideoID+'" frameborder="0" autoplay="1" allowfullscreen></iframe>');	
     $('#videoPlayer').append(embeddedVideoPlayer);
 }
 
